f6fc9a09ac0016156b3eafa658cd3fe8f82aa890
Ashley <garciacervantes.ag@gmail.com>
2024-08-07 13:38:18 -0400

    catkin_ws

--- build/rosjava_test_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashley_garcia/catkin_ws/src/rosjava_test_msgs
-# Build directory: /home/ashley_garcia/catkin_ws/build/rosjava_test_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_isolated/rosjava_core/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashley_garcia/catkin_ws/src/rosjava_core
-# Build directory: /home/ashley_garcia/catkin_ws/build_isolated/rosjava_core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
