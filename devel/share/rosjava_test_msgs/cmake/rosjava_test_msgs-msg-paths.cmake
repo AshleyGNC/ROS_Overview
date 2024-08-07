fefc41bce4e131a19153fdf7fcd7f0aa2741dd2a
Ashley Nathalie Garcia Cervantes <68672421+AshleyGNC@users.noreply.github.com>
2024-08-07 15:18:09 -0400

    Delete devel directory

--- devel/share/rosjava_test_msgs/cmake/rosjava_test_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(rosjava_test_msgs_MSG_INCLUDE_DIRS "/home/ashley_garcia/catkin_ws/src/rosjava_test_msgs/msg")
-set(rosjava_test_msgs_MSG_DEPENDENCIES std_msgs)
